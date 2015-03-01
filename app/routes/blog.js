7ee4354cdf82f1350b4d6cbbaf07289ff94db250
Trey Phillips <tlphillipsjr@gmail.com>
2015-03-01 15:59:27 -0500

    Hey dustin-- look at this

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return Ember.$.ajax("https://api.parse.com/1/classes/posts").then(function(response) {
+      return response.results;
 
+    });
+  }
 });
